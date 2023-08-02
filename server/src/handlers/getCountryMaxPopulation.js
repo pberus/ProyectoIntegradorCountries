264c55b20037d74f320b362e856fe42b9593f9a2
pberus <pedroberustein03@hotmail.com>
2023-08-02 19:46:36 -0300

    arreglar caso en vivo

---
+++ server/src/handlers/getCountryMaxPopulation.js
@@ -0,0 +1,8 @@
+const getCountryMaxPopulationController = require("../controllers/getCountryMaxPopulation")
+
+const getCountryMaxPopulationHandler = async(req, res)=>{
+    const country = await getCountryMaxPopulationController()
+    res.json(country)
+}
+
+module.exports = getCountryMaxPopulationHandler
--- server/src/routes/countries.js
+++ server/src/routes/countries.js
@@ -1,12 +1,14 @@
 const getAllCountriesHandler = require("../handlers/getAllCountries");
 const getCountryByIdHandler = require("../handlers/getCountryById");
 const getCountryByNameHandler = require("../handlers/getCountryByName");
+const getCountryMaxPopulationHandler = require("../handlers/getCountryMaxPopulation");
 
 const routerCountries = require("express").Router();
 
 routerCountries.get("/", getAllCountriesHandler);
 routerCountries.get("/name", getCountryByNameHandler);
 routerCountries.get("/:idPais", getCountryByIdHandler);
+routerCountries.get("/maxpopulation", getCountryMaxPopulationHandler)
 
 
 module.exports = routerCountries;
