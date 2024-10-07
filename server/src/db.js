88121ec72ab446d5f9af013dae206f8c9617bbdf
Pedro <pedroberustein03@hotmail.com>
2024-10-07 20:58:51 -0300

    fix credentials

--- server/src/db.js
+++ server/src/db.js
@@ -3,16 +3,15 @@ const { Sequelize } = require("sequelize");
 
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } = process.env;
 
-const sequelize = new Sequelize(
-  process.env.DB_URL ||
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
-  {
-    logging: false,
-    native: false,
-  }
-);
+const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: 'postgres',
+  logging: false, 
+  native: false,
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
